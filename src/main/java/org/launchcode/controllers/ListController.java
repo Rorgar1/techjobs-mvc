f3b92604ecbbc034bebe68fb6031a942d632ddb6
Robin Bailey <rorgar1@yahoo.com>
2019-08-24 12:44:44 -0500

    all three tasks drafted

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -44,7 +44,8 @@ public class ListController {
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
-            model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
+            model.addAttribute("title", "All "
+                    + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
             return "list-column";
@@ -57,7 +58,8 @@ public class ListController {
             @RequestParam String column, @RequestParam String value) {
 
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
-        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        model.addAttribute("title", "Jobs with "
+                + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
 
         return "list-jobs";
